4684d6c12d5d030d0fb9199afdf2e967bcd150ce
Fabi3210 <66134715+Fabi3210@users.noreply.github.com>
2021-12-11 19:13:01 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+// var ele = document.createElement("script");
+// var path = "C:/Users/Fabian/VSCode/website test/main.js"
+// ele.setAttribute("src",path);
+// document.head.appendChild(ele) 
+
+window.onload = function() {
+    let admin = document.getElementById('adminA')
+    admin.style.visibility = 'visible'
+}
